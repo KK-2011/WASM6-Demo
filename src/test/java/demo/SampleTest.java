dbdf0ef444cf7bd74d906f34eeaa6baa5e5b6b12
KRISHNA KANTH <krishna2011.kanth@gmail.com>
2022-11-21 10:44:30 +0530

    Commit - Sample Test

---
+++ src/test/java/demo/SampleTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleDemo()
+	{
+		System.out.println("Hi, this is the practise of importing and cloning");
+	}
+}
