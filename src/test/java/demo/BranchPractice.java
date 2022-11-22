2453f4829001c6fcbd8682474133c2201a0bc405
KRISHNA KANTH <krishna2011.kanth@gmail.com>
2022-11-22 11:19:48 +0530

    Commit -1 - Performing TC_ID_01 branch operation

---
+++ src/test/java/demo/BranchPractice.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class BranchPractice {
+
+	@Test
+	public void branchPractice()
+	{
+		System.out.println("Hi, this is the practice of branch operation");
+	}
+}
